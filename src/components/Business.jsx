9f33a093fb3c358887e1e5c1121e1904f7dfcfbc
sold1er1K <sold1erus@yandex.ru>
2024-04-30 19:27:30 +0000

    business and billing sections

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import styles, { layout } from "../style"
+import { features } from "../constants"
+import Button from './Button';
+
+const FeautureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-notmal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+)
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          <div className="flex justify-center md:inline">You do the business, </div>
+          <br className="md:block hidden"/> <div className="flex justify-center md:inline">we’ll handle the money.</div>
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+
+        <Button styles="mt-10"/>
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+          {
+            features.map((feature, index) => (
+              <FeautureCard key={feature.id} {...feature} index={index}/>
+            ))
+          }
+        </div>
+    </section>
   )
 }
 
--- src/style.js
+++ src/style.js
@@ -3,7 +3,7 @@ const styles = {
     heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
     paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",  
     flexCenter: "flex justify-center items-center",
-    flexStart: "flex justify-center items-start",
+    flexStart: "flex justify-center md:items-start items-center",
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-12 py-4",  
@@ -18,6 +18,5 @@ export const layout = {
     sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
     sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
 };
-
 
 export default styles;

fb82abbf174b1adf5be1b858d636082c67b5e09b
sold1er1K <sold1erus@yandex.ru>
2024-04-30 20:30:43 +0000

    card deal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import styles from "../style"
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rouned-[10px]`}>
       Get Started
     </button>
   )
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { card } from "../assets"
+import styles, { layout } from "../style"
+import Button from "./Button"
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+      <div className="flex justify-center md:inline">Find a better card deal</div> <br className="md:block hidden"/>
+      <div className="flex justify-center md:inline">in few easy steps.</div>
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+      </p>
+      <Button styles="mt-10"/>  
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+
+    </div>
+  </section>
+)
 
 export default CardDeal
